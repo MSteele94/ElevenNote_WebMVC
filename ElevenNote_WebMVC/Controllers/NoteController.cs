4c3faddd46c88789c9309cf5d5dd8dedd101a8b3
MSteele94 <bengalsfan620@gmail.com>
2021-05-05 14:42:56 -0400

    Added to Note Controller

--- ElevenNote_WebMVC/Controllers/NoteController.cs
+++ ElevenNote_WebMVC/Controllers/NoteController.cs
@@ -1,5 +1,7 @@
 ﻿using ElevenNote_Models;
+using ElevenNote_Services;
 using ElevenNote_WebMVC.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +16,10 @@ namespace ElevenNote_WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
             return View(model);
         }
         //GET
@@ -26,11 +31,16 @@ namespace ElevenNote_WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
