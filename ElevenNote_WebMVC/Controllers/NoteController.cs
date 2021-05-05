7a8c76ede02225f0cf3935ca2d1448fac6c07db9
MSteele94 <bengalsfan620@gmail.com>
2021-05-05 15:24:20 -0400

    Added to note create index

--- ElevenNote_WebMVC/Controllers/NoteController.cs
+++ ElevenNote_WebMVC/Controllers/NoteController.cs
@@ -31,16 +31,26 @@ namespace ElevenNote_WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+            if (!ModelState.IsValid) return View(model);
 
+            var service = CreateNoteService();
+            if (service.CreateNote(model))
+            {
+                //TempData removes info after it was accessed
+
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Note could not be created.");
+            return View(model);
+        }
+
+        //Added by using Edit > Refactor > ExtractMethod
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
