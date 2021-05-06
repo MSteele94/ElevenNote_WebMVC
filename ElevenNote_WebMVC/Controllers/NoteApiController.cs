8f2e80b2ef9113a79bc604df62948bd2208a47a6
MSteele94 <bengalsfan620@gmail.com>
2021-05-05 22:04:57 -0400

    Adding Star function to dev branch

---
+++ ElevenNote_WebMVC/Controllers/NoteApiController.cs
@@ -0,0 +1,47 @@
+﻿using ElevenNote_Models;
+using ElevenNote_Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote_WebMVC.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Note")]
+    public class NoteApiController : ApiController
+    {
+        private bool SetStarState(int noteId, bool newState)
+        {
+            //Create the service
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            //Get the Note
+            var detail = service.GetNoteById(noteId);
+
+            //Create the NoteEdit model instance with new star state
+            var updateNote =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content,
+                    IsStarred = newState
+                };
+
+            //Return a value indicating whether the update succedede
+            return service.UpdateNote(updateNote);
+        }
+        [Route("{id}/Star")]
+        [HttpPut]
+        public bool ToggleStarOn(int id) => SetStarState(id, true);
+
+        [Route("{id}/Star")]
+        [HttpDelete]
+        public bool ToggleStarOff(int id) => SetStarState(id, false);
+    }
+}
